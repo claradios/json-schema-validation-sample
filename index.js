a5ac3efbb66a3c27d7f995067eab8dfdd60d229c
Clara Dios <clara.dios@guidesmiths.com>
2021-11-29 17:25:07 +0100

    chore: format

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+const emoji  = require('./schemas/emoji.js');
+const emojiCollection = require('./emojiCollection.js');
 
-const emoji  = require('./schemas/emoji.js')
-const emojiCollection = require('./emojiCollection.js')
-
-const Ajv = require("ajv")
-const ajv = new Ajv({ allErrors: true })
+const Ajv = require("ajv");
+const ajv = new Ajv({ allErrors: true });
 
 emojiCollection.forEach( emojiItem => {
-    // loop collection elements for validation
-    const validation = ajv.validate(emoji, emojiItem)
-    console.log('VALIDATION', validation)
-    validation
+  // loop collection elements for validation
+  const validation = ajv.validate(emoji, emojiItem);
+  validation
     ? console.log(`emoji: ${emojiItem.symbol} is correctly builded`)
-    : console.log(`emoji: ${emojiItem.symbol} has the following errors: ${JSON.stringify(ajv.errors, null, 2)}`)
+    : console.log(`emoji: ${emojiItem.symbol} has the following errors: ${JSON.stringify(ajv.errors, null, 2)}`);
 });
